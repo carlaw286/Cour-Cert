2b678b382f0d0c83f7f6767fbe29dd52fae786d8
Rancis09 <ryanontal@gmail.com>
2023-09-24 02:03:01 +0800

    HomePage inside Component

---
+++ frontend/src/Components/TeacherHomePage/TeacherHomePage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './TeacherHomePage.css'
+
+
+function TeacherHomePage()
+{
+  return(
+      <div className='homepage'>
+        <nav>
+          <div class ="app-logo">
+            <img src = "/logo/AppLogo.png" alt= "Cour-Cert" height={150} width={100}></img>
+          </div>
+          <div class = "searchBar">
+            <input type = "text" id="search-input" placeholder="Search here"></input>
+            <button id="search-button">Search</button>
+          </div>
+          <div class ="nav-links">
+            <ul>
+              <li><a href = "#"> View Course</a> </li>
+              <li><a href = "#"> Account</a> </li>
+              <li><a href = "#"> Signout</a> </li>
+            </ul>
+          </div>
+        </nav>
+        <div class="container"></div>
+        <div class ="addCourse">
+
+            <button id="add-button">Add Course</button>
+          </div>
+      </div>      
+  )
+}
+
+export default TeacherHomePage;
