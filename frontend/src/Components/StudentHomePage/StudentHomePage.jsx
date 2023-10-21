585cba187fd4b6ca56efb496effb2f38aea04e94
Roger R. Ando <139098175+rogerando1@users.noreply.github.com>
2023-10-21 17:36:17 +0800

    adjusted

--- frontend/src/Components/StudentHomePage/StudentHomePage.jsx
+++ frontend/src/Components/StudentHomePage/StudentHomePage.jsx
@@ -14,7 +14,7 @@ export const StudentHomePage = () =>
             <img src = "logo.png" alt= "Cour-Cert" height={160} width={100}></img>
           </div>
           <div class = "searchBar2">
-            <input type = "text" id="search-input" placeholder="Search here"></input>
+            <input type = "search" id="search-input" placeholder="Search here"></input>
             <button id="search-button">Search</button>
           </div>
           <div class ="nav-link2">
