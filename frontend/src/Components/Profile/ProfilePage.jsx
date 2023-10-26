3a688af56ebcf58e277baf0de86f05f1e95774b1
carlaw286 <carlo.amadeo286@gmail.com>
2023-10-27 02:07:47 +0800

    student profile page (front end)
    edit button and disable textbox

--- frontend/src/Components/Profile/ProfilePage.jsx
+++ frontend/src/Components/Profile/ProfilePage.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProfilePage.css';
 import { useNavigate } from 'react-router-dom';
 
 export const ProfilePage = () => 
 {
   const navigate = useNavigate();
+  // State to track whether the form is in edit mode
+  const [editMode, setEditMode] = useState(false);
+  // State to store form data
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+    // Function to handle input changes
+    const handleInputChange = (e) => {
+      const { id, value } = e.target;
+      setFormData((prevData) => ({
+        ...prevData,
+        [id]: value,
+      }));
+    };
+
+    const toggleEditMode = () => {
+      if (editMode) {
+        // If currently in edit mode, reset form data to initial state on cancel
+        setFormData({
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
+        });
+      }
+      setEditMode(!editMode);
+    };
   return(
     <div className='profilepage'>
       <form>
@@ -31,16 +64,28 @@ export const ProfilePage = () =>
               <p>Last Name</p>
             </div>
             <div className='col-2'>
-                <input type='name' id='firstname' placeholder='Enter first name'>
+                <input type='name' id='firstName' placeholder='Enter first name' 
+                value={formData.firstName}
+                onChange={handleInputChange}
+                disabled={!editMode}> 
+                {/* // Disable input if not in edit mode */}
                 </input>
-                <input type='name' id='firstname' placeholder='Enter first name'>
+                <input type='name' id='lastName' placeholder='Enter last name' 
+                value={formData.lastName}
+                onChange={handleInputChange}
+                disabled={!editMode}> 
+                {/* // Disable input if not in edit mode */}
                 </input>
             </div>
             <div className='col-3'>
               <p>Email</p>
             </div>
             <div className='col-4'>
-              <input type='email' id='email' placeholder='Enter email'>
+              <input type='email' id='email' placeholder='Enter email' 
+                value={formData.email}
+                onChange={handleInputChange}
+                disabled={!editMode}> 
+                {/* // Disable input if not in edit mode */}
               </input>
             </div>
             <div className='col-5'>
@@ -48,9 +93,17 @@ export const ProfilePage = () =>
               <p>Confirm Password</p>
             </div>
             <div className='col-6'>
-                <input type='name' id='firstname' placeholder='Enter password'>
+                <input type='name' id='password' placeholder='Enter password' 
+                value={formData.password}
+                onChange={handleInputChange}
+                disabled={!editMode}> 
+                {/* // Disable input if not in edit mode */}
                 </input>
-                <input type='name' id='firstname' placeholder='Enter confirm password'>
+                <input type='name' id='confirmPassword' placeholder='Enter confirm password' 
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+                disabled={!editMode}> 
+                {/* // Disable input if not in edit mode */}
                 </input>
             </div>
 
@@ -60,7 +113,12 @@ export const ProfilePage = () =>
               </button>
               </div>
               <div className='but2'>
-                <button type='submit' id='updatepassword'> Update
+            <button type='button' id='edit' onClick={toggleEditMode}>
+              {editMode ? 'Cancel' : 'Edit'}
+            </button>
+            </div>
+              <div className='but3'>
+              <button type='submit' id='update' disabled={!editMode}> Update
               </button>
               </div>
             </div>
