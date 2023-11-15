bd3988ded025980244ec2aa555cfc1b8421aab77
Rancis09 <108780181+Rancis09@users.noreply.github.com>
2023-11-15 22:19:32 +0800

    Update TeacherHomePage.jsx
    revert

--- frontend/src/Components/TeacherHomePage/TeacherHomePage.jsx
+++ frontend/src/Components/TeacherHomePage/TeacherHomePage.jsx
@@ -86,19 +86,19 @@ export const TeacherHomePage = () =>
     setOpen(false);
   };
   const navigate = useNavigate();
-  //jwt
-    // axios.defaults.withCredentials = true;
-    // useEffect(()=> {
-    //   axios.get('https://cour-cert-trial.vercel.app/teacherhomepage')
-    //   .then(result => {console.log(result)
-    //       if(result.data !== "Success")
-    //       {
-    //           navigate('/loginsignup')
-    //       }
-    //   })
-    //   .catch(err=> console.log(err))
-    // }, [])
-  //jwt
+  jwt
+    axios.defaults.withCredentials = true;
+    useEffect(()=> {
+      axios.get('http://localhost:3002/teacherhomepage')
+      .then(result => {console.log(result)
+          if(result.data !== "Success")
+          {
+              navigate('/loginsignup')
+          }
+      })
+      .catch(err=> console.log(err))
+    }, [])
+  jwt
   const handleSignout = async () => {
     localStorage.removeItem('token');
     try {
