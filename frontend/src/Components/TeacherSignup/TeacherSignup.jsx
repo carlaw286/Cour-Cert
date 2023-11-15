be6e852c914c6b6fc207067aa7efef95367f385b
Rancis09 <108780181+Rancis09@users.noreply.github.com>
2023-11-15 22:20:31 +0800

    Update TeacherSignup.jsx
    revert

--- frontend/src/Components/TeacherSignup/TeacherSignup.jsx
+++ frontend/src/Components/TeacherSignup/TeacherSignup.jsx
@@ -30,7 +30,7 @@ export function TeacherSignup()
 
     try {
       //backend website for database storing
-      const response = await axios.post('https://cour-cert-trial.vercel.app/teachersignup', {
+      const response = await axios.post('http://localhost:3002/teachersignup', {
         firstName,
         lastName,
         email,
