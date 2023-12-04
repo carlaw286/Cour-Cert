60d06e941b8380d71608b4a0206661fbee2ab644
carlaw286 <carlo.amadeo286@gmail.com>
2023-12-04 09:55:50 +0800

    pdf

--- frontend/src/Components/CourseViewPage/CourseViewPage.jsx
+++ frontend/src/Components/CourseViewPage/CourseViewPage.jsx
@@ -1,41 +1,52 @@
+
 import React, { useEffect, useState } from 'react';
-import './CourseViewPage.css'
+import './CourseViewPage.css';
 import { Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import Quiz from './Quiz';
 
 export const CourseViewPage = () => {
-    const location = useLocation();
-    const queryParams = new URLSearchParams(location.search);
-    const courseTitle = queryParams.get('title');
-    const courseDescription = queryParams.get('description');
-    const id = queryParams.get('id');
-    const [allFiles, setAllFiles] = useState();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const courseTitle = queryParams.get('title');
+  const courseDescription = queryParams.get('description');
+  const id = queryParams.get('id');
+  const [allFiles, setAllFiles] = useState();
 
-    const showPdf = (file) => {
-        window.open(`http://localhost:3002/uploaded-files/${file}`, "_blank", "noreferrer");
+  const showPdf = (file) => {
+    const pdfContainer = document.getElementById('pdfContainer');
+    const iframe = document.createElement('iframe');
+    iframe.src = `http://localhost:3002/uploaded-files/${file}`;
+    iframe.height = '50%'; // Match the height with your CSS value
+    iframe.title = 'PDF Viewer';
+  
+    // Set marginLeft value
+    iframe.style.marginLeft = '25%'; // Replace 'your-desired-margin-left-value' with the desired value
+    iframe.style.marginRight = '25%';
+    pdfContainer.innerHTML = '';
+    pdfContainer.appendChild(iframe);
+  };
+
+  useEffect(() => {
+    const getPDF = async () => {
+      try {
+        const response = await axios.get('http://localhost:3002/getWeeklyTopics', {
+          params: {
+            id,
+          },
+        });
+        setAllFiles(response.data); // Assuming response.data is an array of topics
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
     };
 
-    useEffect(() => {
-        const getPDF = async () => {
-            try {
-                const response = await axios.get('http://localhost:3002/getWeeklyTopics', {
-                    params: {
-                        id
-                    }
-                });
-                setAllFiles(response.data); // Assuming response.data is an array of topics
-            } catch (error) {
-                console.error("Error fetching data: ", error);
-            }
-        };
+    getPDF(); // Fetch data when the component mounts or 'id' changes
+  }, [id]);
 
-        getPDF(); // Fetch data when the component mounts or 'id' changes
-    }, [id]);
-
-    return (
-        <div className='addcoursecontainer1'>
-            <nav className='first-nav1'>
+  return (
+    <div className='addcoursecontainer1'>
+      <nav className='first-nav1'>
                 <div className="first-nav-logo1">
                     <Link to='/studenthomepage'>
                         <img src="Logo1.1.png" alt="Cour-Cert"></img>
@@ -56,56 +67,53 @@ export const CourseViewPage = () => {
                     </ul>
                 </div>
             </nav>
-
-            <div className='containerss'>
-                <div className='coursetitle'>
-                    {courseTitle}
-                </div>
-                <div className='coursedescriptions'>
-                    <p>
-                        {courseDescription}
-                    </p>
-                </div>
-                {allFiles == null
-                    ? ""
-                    : (() => {
-                        // Grouping files by week number
-                        const groupedFiles = allFiles.reduce((acc, curr) => {
-                            const existingWeek = acc.find(item => item.weekNumber === curr.weekNumber);
-                            if (existingWeek) {
-                                existingWeek.files.push(curr);
-                            } else {
-                                acc.push({
-                                    weekNumber: curr.weekNumber,
-                                    files: [curr],
-                                });
-                            }
-                            return acc;
-                        }, []);
-
-                        // Sort the grouped files by week number
-                        groupedFiles.sort((a, b) => parseInt(a.weekNumber) - parseInt(b.weekNumber));
-
-                        return groupedFiles.map((weekData, index) => (
-                            <div className='cotainerss' key={index}>
-                                <div className='weeknumbers'>
-                                    Week: {weekData.weekNumber}
-                                </div>
-                                {weekData.files.map((fileData, fileIndex) => (
-                                    <div className='weeklyfiles' key={fileIndex} onClick={() => showPdf(fileData.file)}>
-                                        {fileData.PDFdescription}
-                                    </div>
-                                ))}
-                            </div>
-                        ));
-                    })()
-                }
-                <div>
-                    <p>
-                    <Quiz />
-                    </p>
-                </div>
-            </div>
+      <div className='containerss'>
+        <div className='coursetitle'>{courseTitle}</div>
+        <div className='coursedescriptions'>
+          <p>{courseDescription}</p>
         </div>
-    );
+        <div id='pdfContainer' className='pdf-container'></div>
+        {allFiles == null
+          ? ''
+          : (() => {
+              // Grouping files by week number
+              const groupedFiles = allFiles.reduce((acc, curr) => {
+                const existingWeek = acc.find((item) => item.weekNumber === curr.weekNumber);
+                if (existingWeek) {
+                  existingWeek.files.push(curr);
+                } else {
+                  acc.push({
+                    weekNumber: curr.weekNumber,
+                    files: [curr],
+                  });
+                }
+                return acc;
+              }, []);
+
+              // Sort the grouped files by week number
+              groupedFiles.sort((a, b) => parseInt(a.weekNumber) - parseInt(b.weekNumber));
+
+              return groupedFiles.map((weekData, index) => (
+                <div className='cotainerss' key={index}>
+                  <div className='weeknumbers'>Week: {weekData.weekNumber}</div>
+                  {weekData.files.map((fileData, fileIndex) => (
+                    <div
+                      className='weeklyfiles'
+                      key={fileIndex}
+                      onClick={() => showPdf(fileData.file)}
+                    >
+                      {fileData.PDFdescription}
+                    </div>
+                  ))}
+                </div>
+              ));
+            })()}
+        <div>
+          <p>
+            <Quiz />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
