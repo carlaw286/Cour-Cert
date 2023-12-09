b9a99e7d7f6e9d57a2f09a1f336559a95c62021c
serenity2423 <ma.leahlyn.fernandez@gmail.com>
2023-12-09 15:01:07 +0800

    added changes on certificate design

--- frontend/src/Components/Certificate/Certificate.jsx
+++ frontend/src/Components/Certificate/Certificate.jsx
@@ -1,39 +1,28 @@
-import './Certificate.css';
+import './Certificate1.css';
 
-export function Certificate({ name, course, date }) {
+export function Certificate({ name, course, date,instructor }) {
   return (
     <div className="App">
-      <Icon />
-      <p className="byline">Certificate of completion</p>
-
+      <p className="byline"></p>
       <div className="content">
-        <p>Awarded to</p>
+        <p>This certificate is presented to</p>
         <h1>{name}</h1>
-        <p>for completing:</p>
-        <h2>{course}</h2>
+        <p>for successfully demonstrating exceptional knowledge and proficiency by passing<p>
+          </p> the course: {course}. Issued on {date} ,<p>
+            </p> this accomplishment signifies [his/her] commitment to excellence, dedication<p>
+              </p> to continuous learning, and mastery of the course content. </p>
+        <h2>{instructor}</h2>
       </div>
-
-      {date && (
-        <p className="date">
-          Issued on{' '}
-          <span className="bold">{date}</span>
-        </p>
-      )}
     </div>
   );
 }
 
 Certificate.defaultProps = {
   name: 'James Lee',
-  course: 'Creating PDFs with React & Make.cm',
-  date: 'March 15 2021'
+  course: 'Data Structures and Algorithms',
+  date: 'March 15 2021',
+  instructor: 'Engr. James Yohan Curises'
 }
 
-const Icon = () => (
-  <svg width="99" height="139" viewBox="0 0 99 139" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 0H99V138.406L52.1955 118.324L0 138.406V0Z" fill="white" />
-    <path d="M25.4912 83.2515C25.4912 79.4116 27.0222 75.7289 29.7474 73.0137C32.4727 70.2985 36.1689 68.7731 40.0229 68.7731C43.877 68.7731 47.5732 70.2985 50.2984 73.0137C53.0236 75.7289 54.5546 79.4116 54.5546 83.2515M40.0229 59.724C40.0229 55.8841 41.5539 52.2014 44.2791 49.4862C47.0044 46.7709 50.7006 45.2455 54.5546 45.2455C58.4087 45.2455 62.1049 46.7709 64.8301 49.4862C67.5553 52.2014 69.0863 55.8841 69.0863 59.724V83.2515" stroke="#0379FF" strokeWidth="10.6193" />
-  </svg>
-)
 
 
