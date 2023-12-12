d934351d20a8c0d9648490e4cbf8e58586a2cbe6
serenity2423 <ma.leahlyn.fernandez@gmail.com>
2023-12-12 16:04:18 +0800

    Download Certificate
    -can generate a pdf file when download button is clicked
    
    -need some fixing pa ang makita sa pdf file though tungod guros sizing, basta need pa i adjust

--- frontend/src/Components/Certificate/Certificate.jsx
+++ frontend/src/Components/Certificate/Certificate.jsx
@@ -1,8 +1,32 @@
+import React, { useRef } from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 import './Certificate1.css';
 
-export function Certificate({ name, course, date,instructor }) {
+export function Certificate({ name, course, date, instructor }) {
+  const certificateRef = useRef(null);
+
+  const handleDownload = async () => {
+    const certificateElement = certificateRef.current;
+
+    if (certificateElement) {
+      // Capture the content of the certificate
+      const desiredSizePercentage = 90; // Adjust this value as needed
+      const scaleFactor = desiredSizePercentage / 100;
+
+      const canvas = await html2canvas(certificateElement, { scale: scaleFactor });
+
+      // Create a PDF document
+      const pdf = new jsPDF('landscape', 'mm', 'a4');
+      pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 210, 297);
+
+      // Download the PDF
+      pdf.save('certificate.pdf');
+    }
+  };
+
   return (
-    <div className="App">
+    <div className="App" ref={certificateRef}>
       <p className="byline"></p>
       <div className="content">
         <p>This certificate is presented to</p>
@@ -12,6 +36,9 @@ export function Certificate({ name, course, date,instructor }) {
             </p> this accomplishment signifies [his/her] commitment to excellence, dedication<p>
               </p> to continuous learning, and mastery of the course content. </p>
         <h2>{instructor}</h2>
+        <button className="download-button" onClick={handleDownload}>
+        Download
+      </button>
       </div>
     </div>
   );
